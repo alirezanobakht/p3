6ef36697869d82f173ae3cf80489db41fb5c93eb
Alireza Nobakht <me@me.com>
2018-12-23 14:40:43 -0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/p3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p3.dir/main.cpp.obj"
+  "p3.pdb"
+  "p3.exe"
+  "p3.exe.manifest"
+  "libp3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
